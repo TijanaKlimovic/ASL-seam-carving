f8752eee04f12bd5c654fd9c933d252cdd718e87
Aydin <afaraji@student.ethz.ch>
2020-03-15 22:04:50 +0100

    test()

--- min_seam.c
+++ min_seam.c
@@ -65,6 +65,46 @@ void min_seam(int rsize, int csize, const double **img, const double **e1, doubl
 
 }
 
+void test() {
+	double a[] = {4.1, 2.3, 5.5, 4.5, 8.2, 0.9, 10, 1.2, 3.3, 3, 3, 2, 9.0};
+	int idx;
+	double m = min(a, 13, &idx);
+	printf("[%d] = %lf\n", idx, m);
+	double **mat = create_2d_array(5, 3);
+
+	for (int i = 0; i < 5; i++) {
+
+		for (int j = 0; j < 3; j++) {
+			mat[i][j] = (i * 3 + j) / 2.0;
+		}
+
+	}
+
+	for (int i = 0; i < 5; i++) {
+
+		for (int j = 0; j < 3; j++) {
+			printf("%lf ", mat[i][j]);
+		}
+
+		printf("\n");
+	}
+
+	double am[5][3] = {4.1, 2.3, 5.5, 4.5, 8.2, 0.9, 10, 1.2, 3.3, 3, 3, 2, 9.0, 4, -2.4};
+
+	for (int i = 0; i < 5; i++) {
+		printf("%p: \n", am[i]);
+		printf("%p: \n", am[i][0]);
+
+		for (int j = 0; j < 3; j++) {
+			printf("%lf ", am[i][j]);
+		}
+
+		printf("\n");
+	}
+	
+}
+
 int main(int argc, char const *argv[]) {
+
 	return 0;
 }
