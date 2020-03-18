d3edc965b83f3caba2c3c32f07554b5eb4562389
Dora <ndori0713@gmail.com>
2020-03-18 16:22:51 +0100

    Changed interface

--- parse_img.c
+++ parse_img.c
@@ -1,4 +1,5 @@
 // compile with : gcc -Wall parse_img.c  -o parse_img -lm
+// usage: ./parse_img <filename>
 
 #include <stdint.h>
 #include <stdio.h>
@@ -6,34 +7,50 @@
 
 #define STB_IMAGE_IMPLEMENTATION
 #include "stb_image.h"
+#define STB_IMAGE_WRITE_IMPLEMENTATION
+#include "stb_image_write.h"
 
 #define C (3)
 
-typedef unsigned char u8;
-
 int width, height;
-u8 *original, *r, *g, *b;
+unsigned char *original, *saved;
+double *output;
 
-void copy_rgb(u8 *r, u8 *g, u8 *b, u8 *src) {
-	for (int i = 0; i < height; ++i) {
-		for (int j = 0; j < width; ++j) {
-			for (int k = 0; k < C; ++k) {
-				// red
-				if (k % C == 0)
-					r[i * width + j] = src[i * width * C + j * C + k];
-				// green
-				else if (k % C == 1)
-					g[i * width + j] = src[i * width * C + j * C + k];
-				// blue
-				else if (k % C == 2)
-					b[i * width + j] = src[i * width * C + j * C + k];
+void convert_double(unsigned char *src, double *dst) {
+	int i, j, k;
+	for (i = 0; i < height; ++i) {
+		for (j = 0; j < width; ++j) {
+			for (k = 0; k < C; ++k) {
+				dst[i * width * C + j * C + k] = 
+				(double)src[i * width * C + j * C + k];
 			}
 		}
 	}
 }
 
-int allocate_buffer(int width, int height, u8 **buffer) {
-	*buffer = malloc(width * height * sizeof(u8));
+void convert_uchar(double *src, unsigned char *dst) {
+	int i, j, k;
+	for (i = 0; i < height; ++i) {
+		for (j = 0; j < width; ++j) {
+			for (k = 0; k < C; ++k) {
+				dst[i * width * C + j * C + k] = 
+				(unsigned char)src[i * width * C + j * C + k];
+			}
+		}
+	}
+}
+
+int allocate_double_buffer(int width, int height, double **buffer) {
+	*buffer = malloc(width * height * C * sizeof(double));
+	if (*buffer == NULL) {
+		printf("Failed to allocate buffer\n");
+		return 0;
+	}
+	return 1;
+}
+
+int allocate_uchar_buffer(int width, int height, unsigned char **buffer) {
+	*buffer = malloc(width * height * C * sizeof(unsigned char));
 	if (*buffer == NULL) {
 		printf("Failed to allocate buffer\n");
 		return 0;
@@ -50,12 +67,18 @@ int load_image(char *filename) {
 	}
 	assert(n == C);
 	printf("Loaded image (%d x %d)\n", width, height);
-	if (!allocate_buffer(width, height, &r) 
-	|| !allocate_buffer(width, height, &g)
-	|| !allocate_buffer(width, height, &b)) 
+	if (!allocate_double_buffer(width, height, &output)) 
 		return 0;
-	copy_rgb(r, g, b, original);
+	printf("Allocated buffer\n");
+	convert_double(original, output);
+	printf("Converted to double\n");
 	return 1;
+}
+
+void save_image(char *filename) {
+	allocate_uchar_buffer(width, height, &saved);
+	convert_uchar(output, saved);
+	stbi_write_png(filename, width, height, C, saved, width * C);
 }
 
 int main(int argc, char **argv) {
@@ -65,5 +88,6 @@ int main(int argc, char **argv) {
 	if (!load_image(argv[1])) {
 		return 1;
 	}
+	save_image("saved.png");
 	return 0;
 }
