58eb48c89ca1779afefa36c7d53f999d4cc9ae34
Ioana Stefan <iostefan@student.ethz.ch>
2020-04-24 23:03:05 +0300

    [VALIDATION] Updated main to match interface with python reference code.

--- main.c
+++ main.c
@@ -8,6 +8,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <time.h>
+#include <math.h>
 
 #include "tsc_x86.h"
 
@@ -62,9 +63,39 @@ int run(const char* path,const char* output_file_name, int width_diff, int heigh
 	int width, height;
 	double *output;
 
+	if (!load_image(path, &width, &height, &output)) {
+		return 1;
+	}
+
+	output = optimal_image(width, height, width_diff, height_diff, output);
+	//printf("Finished seam carving\n");
+	save_image(output_file_name, width - width_diff, height - height_diff, output);
+	//printf("Saved image as %s with size (%d, %d) \n", output_file_name, width - width_diff, height - height_diff);
+	// free(output);
+	return 0;
+}
+
+
+/* 
+takes in the image path and runs the seam carving algorithm
+conforms to the reference python interface
+*/
+
+int run_python_validation(const char* path,const char* output_file_name, const char* col_row,
+	double percentage) {
+	int width, height;
+	double *output;
 
 	if (!load_image(path, &width, &height, &output)) {
 		return 1;
+	}
+
+	int width_diff = 0, height_diff = 0;
+
+	if (strcmp(col_row, "c")) {
+		height_diff = ceil(height * (1 - 0.01*percentage));
+	} else {
+		width_diff = ceil(width * (1 - 0.01*percentage));
 	}
 
 	output = optimal_image(width, height, width_diff, height_diff, output);
@@ -115,7 +146,15 @@ double rdtsc(const char* path, const char* output_file_name, int width_diff, int
 /* main function , has two options, first is the image path second is -t/<anything except nothing> */ 
 
 int main(int argc, char const *argv[]) {
-	
+
+	if (argc == 5) {
+		// verification run - python interface
+		printf("Usage should be: %s <image_path> <output_file_name> <c/r> <percentage>\n", argv[0]);
+
+		double percentage = atoi(argv[4]);
+		return run_python_validation(argv[1], argv[2], argv[3], percentage);
+	}
+
 	if (argc < 6) {
 		printf("Usage: %s <image_path> <output_file_name> <width_diff> <height_diff> <timing boolean>\n", argv[0]);
 		return 1;
