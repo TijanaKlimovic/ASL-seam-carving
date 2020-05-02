6d342c125b7dc4c7f81fd3a3686f13aab1400dac
Aydin <afaraji@student.ethz.ch>
2020-05-02 14:14:09 +0200

    directory processing

--- batch_run/br.sh
+++ batch_run/br.sh
@@ -11,33 +11,72 @@ h_step=200 #height step (for iteration); h = h - h_step
 
 gcc_flags="-Ofast -Wall -lm -g"
 
-timing=1 #passed to ./seam_carving
+timing=0 #passed to ./seam_carving
 #--------------------------------------------------------
 #END OF PARAMS [DON'T EDIT THE REST OF THE CODE]
 
 
 if [ "$#" -ne 3 ]
 then
-	echo "usage: ./br.sh <img_file> <src_dir> <output_file.txt>"
+	echo "usage: ./br.sh <img_file/img_folder> <src_dir> <output_file.txt>"
 	exit
 fi
 
-img="$1"
-fname=$(basename "$img")
+if [ -f "$1" ]
+then
+	# single image passed
+	files=("$1")
+	f_step=0
+	width=$(identify "$1" | awk '{print $3}' | tr "x" "\n" | head -n 1)
+	height=$(identify "$1" | awk '{print $3}' | tr "x" "\n" | tail -n 1)
+elif [ -d "$1" ]
+then
+	# directory passed
+	files=()
+	width=99999999
+	height="$width"
+
+	for f in "$1"/*.png
+	do
+		files+=("$f")
+		tmp_w=$(identify "$f" | awk '{print $3}' | tr "x" "\n" | head -n 1)
+		tmp_h=$(identify "$f" | awk '{print $3}' | tr "x" "\n" | tail -n 1)
+
+		if [ "$tmp_w" -lt "$width" ]
+		then
+			width=$tmp_w
+		fi
+
+		if [ "$tmp_h" -lt "$height" ]
+		then
+			height=$tmp_h
+		fi
+
+	done
+
+	f_step=1
+else
+	echo "$1 doesn't exist"
+	echo "usage: ./br.sh <img_file/img_folder> <src_dir> <output_file.txt>"
+	exit
+fi
+
+file_count=${#files[@]}
+
 printf "%-10s %10s %10s %10s %10s %20s\n" "filename" "width" "height" "w_diff" "h_diff" "time[cycles]" >> "$3"
 
 gcc "$2"/*.c $gcc_flags -o seam_carving
 
-width=$(identify "$img" | awk '{print $3}' | tr "x" "\n" | head -n 1)
-height=$(identify "$img" | awk '{print $3}' | tr "x" "\n" | tail -n 1)
-
 w="$width"
 h="$height"
+f_idx=0
 mkdir inputs
 mkdir outputs
 
-while (( w > 0 && h > 0 ))
+while (( w > 0 && h > 0 && f_idx < file_count ))
 do
+	img="${files[f_idx]}"
+	fname=$(basename "$img")
 	new_name="$w"x"$h""_$fname"
 	convert "$img" -resize "$w"x"$h"\! "inputs/$new_name"
 	w_red=$(echo "$w * $w_red_p" | bc | tr "." "\n" | head -n 1)
@@ -45,6 +84,12 @@ do
 	echo "./seam_carving inputs/$new_name outputs/$new_name $w_red $h_red $timing"
 	result=$(./seam_carving "inputs/$new_name" "outputs/$new_name" "$w_red" "$h_red" "$timing" | awk '{print $3}') #time
 	printf "%-10s %10d %10d %10d %10d %20d\n" "$fname" "$w" "$h" "$w_red" "$h_red" "$result" >> "$3"
-	(( w = w - w_step ))
-	(( h = h - h_step ))
+
+	if [ "$f_step" -eq 0 ]
+	then
+		(( w = w - w_step ))
+		(( h = h - h_step ))
+	fi
+
+	(( f_idx = f_idx + f_step ))
 done
