b9ae00e6af3ba9ee0f59f1b5d67be7206ec26205
Aydin <afaraji@student.ethz.ch>
2020-05-02 02:04:43 +0200

    added script for batch running

---
+++ batch_run/sens.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+if [ "$#" -ne 2 ]
+then
+	echo "usage: sens.sh <img_file> <src_dir>"
+	exit
+fi
+
+img="$1"
+
+here=$(pwd)
+
+gcc "$2"/*.c -Ofast -Wall -lm -g -o seam_carving
+
+width=$(identify "$img" | awk '{print $3}' | tr "x" "\n" | head -n 1)
+height=$(identify "$img" | awk '{print $3}' | tr "x" "\n" | tail -n 1)
+
+i="$width"
+fname=$(basename "$img")
+mkdir inputs
+mkdir outputs
+
+while [ "$i" -gt 0 ]
+do
+	new_name="$i"x"$height""_$fname"
+	convert "$img" -resize "$i"x"$height"\! "inputs/$new_name"
+	dim_diff=$(echo "$i * 0.15" | bc)
+	dim_diff=$(printf "%d\n" "$dim_diff") #convert to int
+	# result=$(./seam_carving "inputs/$new_name" "outputs/$new_name" 0 1 | awk '{print $3}') #time
+	echo "./seam_carving inputs/$new_name outputs/$new_name $dim_diff 0 1" #time
+	(( i = i - 200 ))
+done
