d48e0d6e08fa63da9e38cefb0a527ec3b35b4181
Dora Neubrandt <dneubrandt@student.ethz.ch>
2020-05-21 11:33:08 +0100

    Rotate energy map for horizontal min seam removal

--- min_seam.c
+++ min_seam.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <limits.h>
 #include "convolution.h"
+#include "parse_img.h"
 
 //--------------------	counter for instructions -------------------
 
@@ -15,167 +16,134 @@ extern unsigned long long mult_count; 	//count the total number of mult instruct
 
 //------------------------------------------------------------------
 
-#define MIN2(X, Y, M, IDX) if (X < Y) {M = X; IDX = 0;} else {M = Y; IDX = 1;}
 
+// #define MIN2(X, Y, M, IDX) (X < Y) ? (M = X, IDX = 0) : (M = Y, IDX = 1);
+// #define MIN21(X, Y, M, IDX) (X < Y) ? (M = X, IDX = -1) : (M = Y, IDX = 0);
+// #define MIN3(X, Y, Z, M, IDX) (X < Y) && (X < Z) ? (M = X, IDX = -1) : MIN2(Y, Z, M, IDX);
+
+#define MIN2(X, Y, M, IDX) if (X < Y) {M = X; IDX = 0;} else {M = Y; IDX = 1;}
+#define MIN21(X, Y, M, IDX) if (X < Y) {M = X; IDX = -1;} else {M = Y; IDX = 0;}
 #define MIN3(X, Y, Z, M, IDX) if ((X < Y) && (X < Z)) {M = X; IDX = -1;} else {MIN2(Y, Z, M, IDX)}
 
-// #define LOG(X) X
-#define LOG(X)
+void rotate(int *energy, int *rotated, int h, int w) { 
+    for (int i = 0; i < h; i++) { 
+        for (int j = 0; j < w; j++) { 
+            rotated[j * h + (h - i - 1)] = energy[i * w + j]; 
+        } 
+    } 
+}
 
 int min_seam(int rsize, int csize, unsigned char *img, int is_ver, int *ret_backtrack) {
 
-	int *the_m = (int *) malloc(rsize * csize * sizeof(int));
+	int *energy = (int *) malloc(rsize * csize * sizeof(int));
 	int *padded_img = padd0_image(rsize, csize, img); //TODO try converting in pad to uchar
-	calc_RGB_energy(rsize + 2, csize + 2, padded_img, the_m);
+	calc_RGB_energy(rsize + 2, csize + 2, padded_img, energy);
 	// contains index of the value from the prev row/column from where we came here
 	int *backtrack = (int *) malloc(rsize * csize * sizeof(int)); //different from what we returnCOUNT(mult_count, 2)
 
+	// if horizontal seam -> rotate +90 degrees the energy map
+	int *dp;
+	if (is_ver == 0) {
+		dp = malloc(rsize * csize * sizeof(int));
+		rotate(energy, dp, rsize, csize);
+		int tmp = rsize;
+		rsize = csize;
+		csize = tmp;
+		free(energy);
+	} else {
+		dp = energy;	
+	}
+
 	// find vertical min seam
-	if (is_ver) {
-		// traverse matrix in row major order
-		for (int i = 1; i < rsize; i++) { //start from second row	
-			// first col, j == 0
-			int where = i * csize;
-			int where_before = where - csize;
-			int min_idx;
-			int min_val;
+	// traverse matrix in row major order
+	int column_lim = csize - 1;
+	int min_idx;
+	int min_val;
+	for (int i = 1; i < rsize; i++) { //start from second row	
+		// first col, j == 0
+		int row = i * csize;
+		int where = row;
+		int where_before = where - csize;
+		
+		int val1 = dp[where_before];
+		int val2 = dp[where_before + 1];
+		MIN2(val1, 
+			 val2, 
+			 min_val, 
+			 min_idx);
 
-			MIN2(the_m[where_before], 
-						 the_m[where_before + 1], 
-						 min_val, 
-						 min_idx)
+		backtrack[where] = min_idx;
+		dp[where] += min_val;
 
-			backtrack[where] = min_idx;
-			the_m[where] += min_val;
+		for (int j = 1; j < column_lim; j++) {
 
-			for (int j = 1; j < csize - 1; j++) {
-
-				where = i * csize + j;
-				where_before = where - csize;
-
-	
-				MIN3(the_m[where_before - 1], 
-					 the_m[where_before], 
-					 the_m[where_before + 1], 
-					 min_val, 
-					 min_idx)
-
-				backtrack[where] = j + min_idx;
-				the_m[where] += min_val;
-			}
-			// last row, j == csize - 1
-			where = i * csize + csize - 1;
+			where = row + j;
 			where_before = where - csize;
-			MIN2(the_m[where_before - 1],
-						 the_m[where_before],
-						 min_val,
-						 min_idx)
 
-			min_idx--;
-			backtrack[where] = csize - 1 + min_idx;
+			val1 = dp[where_before - 1];
+			val2 = dp[where_before];
+			int val3 =  dp[where_before + 1];
+			MIN3(val1, 
+				 val2, 
+				 val3, 
+				 min_val, 
+				 min_idx)
 
-			the_m[where] += min_val;
+			backtrack[where] = j + min_idx;
+			dp[where] += min_val;
 		}
+		// last row, j == csize - 1
+		where = row + column_lim;
+		where_before = where - csize;
+		
+		val1 = dp[where_before - 1];
+		val2 =  dp[where_before];
+		MIN21(val1,
+			 val2,
+			 min_val,
+			 min_idx)
 
-		//process the data to return in appropriate format
-		int ret = INT_MAX;
-		int direction = -1; //used in turning 2D backtrack into 1D
+		backtrack[where] = column_lim + min_idx;
+		dp[where] += min_val;
+	}
 
-		//find the index of the minimum value of last row in the dp matrix
-		int last_row = (rsize - 1)  * csize;
-		for (int cnt = 0; cnt < csize; cnt++) {
-			int current = last_row + cnt;
-			if (the_m[current] < ret) {
-				ret = the_m[current];
-				direction = cnt;
-			}
+	//process the data to return in appropriate format
+	int ret = INT_MAX;
+	int direction = -1; //used in turning 2D backtrack into 1D
+	int row_lim = rsize - 1;
+
+
+	//find the index of the minimum value of last row in the dp matrix
+	int last_row = row_lim  * csize;
+	for (int cnt = 0; cnt < csize; cnt++) {
+		int current = last_row + cnt;
+		int min = dp[current];
+		if (min < ret) {
+			ret = min;
+			direction = cnt;
 		}
+	}
 
-		//return the 1D backtrack (only the min seam)
-		// direction -= last_start;
+	//return the 1D backtrack (only the min seam)
+	// direction -= last_start;
 
-		for (int i = rsize - 1; i >= 0; i--) {
+	if (is_ver == 1) {
+		for (int i = row_lim; i >= 0; i--) {
 			ret_backtrack[i] = direction;
 			direction = backtrack[last_row + direction];
 			last_row -= csize;
 		}
-		free(the_m);
-		free(backtrack);
-		free(padded_img);		
-		return ret;
-
 	} else {
-	// find horizontal min seam
-		for (int i = 1; i < csize; i++) { //start from second col
-			
-			int where = i;
-			int where_before = where - 1;
-			int min_idx;
-			int min_val;
-
-			// j == 0
-			MIN2(the_m[where_before], 
-						 the_m[where_before + csize], 
-						 min_val, 
-						 min_idx)
-
-			backtrack[where] = min_idx;
-			the_m[where] += min_val;
-
-			for (int j = 1; j < rsize - 1; j++) {
-
-				where = j * csize + i;
-				where_before = where - 1;
-
-				MIN3(the_m[where_before - csize], 
-					 the_m[where_before], 
-					 the_m[where_before + csize], 
-					 min_val, 
-					 min_idx)
-
-				backtrack[where] = j + min_idx;
-				the_m[where] += min_val;
-			}
-			// last row
-			where = (rsize - 1) * csize + i;
-			where_before = where - 1;
-			MIN2(the_m[where_before - csize],
-						 the_m[where_before],
-						 min_val,
-						 min_idx)
-
-			min_idx--;
-
-			backtrack[where] = rsize - 1 + min_idx;
-			the_m[where] += min_val;
+		//convert back indexes
+		for (int i = row_lim; i >= 0; i--) {
+			int d = column_lim - direction;
+			ret_backtrack[i] = d;
+			direction = backtrack[last_row + direction];
+			last_row -= csize;
 		}
-		//process the data to return in appropriate format
-		int ret = INT_MAX;
-		int direction = -1; //used in turning 2D backtrack into 1D
-
-		//find the minimum of last row/col of the_m
-		//set the counters to the beginning of the last row/col
-		int last_col = csize - 1;
-		
-		for (int cnt = 0; cnt < rsize; cnt++) {
-			int current = last_col + (cnt * csize);
-			if (the_m[current] < ret) {
-				ret = the_m[current];
-				direction = cnt;
-			}
-		}
-
-		//return the 1D backtrack (only the min seam)
-		// direction -= last_start;
-
-		for (int i = csize - 1; i >= 0; i--) {
-			ret_backtrack[i] = direction;
-			direction = backtrack[last_col + (direction * csize)];
-			last_col -= 1;
-		}
-		free(the_m);
-		free(backtrack);
-		free(padded_img);		
-		return ret;
 	}
+	free(dp);
+	free(backtrack);
+	free(padded_img);		
+	return ret;
 }
