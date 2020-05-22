9a9015606c2788eff933e778cc53f8e2b62fa96d
TijanaKlimovic <tijana.klimovic.tk@gmail.com>
2020-05-22 02:23:27 +0200

    aaaand we fix de block numerino again

--- convolution.c
+++ convolution.c
@@ -39,7 +39,7 @@ void calc_RGB_energy(int n, int m, short* padded, int* energy){
     #endif
     int i_limit = n - K;
 
-    int block_width_L1 = 1065;      //working set size is 4(m+2)+m
+    int block_width_L1 = 1141;      //working set size is 2*3*4(m+2) + m*4 < C
     int width_limit_L1 = m - K - block_width_L1 + 1;
 
     int jj, jj_old;
